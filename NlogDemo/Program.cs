8506dd646804b999573b6f85aa021735dc913f44
Darsh <darshankondhare2698@gmail.com>
2022-11-27 16:10:26 +0530

    [darsh] Add: Nlog Project

---
+++ NlogDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace NlogDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition Problem\n Enter a & b values");
+            Sum Sum = new Sum();
+            Console.WriteLine("Result is : " + " " + Sum.add(0, 1));
+
+
+        }
+    }
+}
---
+++ NlogDemo/Sum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogDemo
+{
+    internal class Sum
+    {
+        private readonly Nlog nLog = new Nlog();
+        public int add(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting null values");
+                nLog.LogWarn("secondNumber or secondNumber should not be 0 ");
+            }
+            int result = firstNumber + secondNumber;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result" + result);
+            return result;
+        }
+    }
+}
